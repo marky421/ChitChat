5b36a72fed763bad4c78f46dbad6bb0789aca8f5
Mark Spain <markyspain421@gmail.com>
2013-08-04 11:12:14 -0700

    First version of Login code

---
+++ ChitChat/src/com/maspain/chitchat/Login.java
@@ -0,0 +1,104 @@
+package com.maspain.chitchat;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+
+public class Login extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane;
+	private JTextField txtName;
+	private JTextField txtAddress;
+	private JLabel lblIpAddress;
+	private JTextField txtPort;
+	private JLabel lblPort;
+	private JLabel lblIPAddressDescription;
+	private JLabel lblPortDescription;
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		
+		setResizable(false);
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 380);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtName = new JTextField();
+		txtName.setBounds(77, 36, 146, 28);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblName = new JLabel("Name:");
+		lblName.setBounds(36, 42, 46, 16);
+		contentPane.add(lblName);
+		
+		txtAddress = new JTextField();
+		txtAddress.setBounds(77, 104, 146, 28);
+		contentPane.add(txtAddress);
+		txtAddress.setColumns(10);
+		
+		lblIpAddress = new JLabel("IP Address:");
+		lblIpAddress.setBounds(6, 110, 76, 16);
+		contentPane.add(lblIpAddress);
+		
+		txtPort = new JTextField();
+		txtPort.setBounds(77, 172, 146, 28);
+		contentPane.add(txtPort);
+		txtPort.setColumns(10);
+		
+		lblPort = new JLabel("Port:");
+		lblPort.setBounds(47, 178, 35, 16);
+		contentPane.add(lblPort);
+		
+		lblIPAddressDescription = new JLabel("(eg. 192.168.0.3)");
+		lblIPAddressDescription.setBounds(95, 132, 110, 16);
+		contentPane.add(lblIPAddressDescription);
+		
+		lblPortDescription = new JLabel("(eg. 8196)");
+		lblPortDescription.setBounds(116, 201, 68, 16);
+		contentPane.add(lblPortDescription);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(91, 294, 117, 29);
+		contentPane.add(btnLogin);
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
